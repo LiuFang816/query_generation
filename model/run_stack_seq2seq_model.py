8fc53c605d89b7e89328ee59634b6567a7ed45c0
Pang <659338505@qq.com>
2018-02-01 19:36:50 +0800

    add models todo: add test(acc效果不好,改为使用bleu)

---
+++ model/run_stack_seq2seq_model.py
@@ -0,0 +1,166 @@
+# -*- coding: utf-8 -*-
+from data.data_reader import *
+from model.stack_seq2seq import *
+import time
+from datetime import timedelta
+import numpy as np
+flags=tf.flags
+flags.DEFINE_string('data_path','../data','data path')
+flags.DEFINE_string('save_path','../data/checkpoints/stack_seq2seq/best_validation','best val save path')
+flags.DEFINE_string('save_dir','../data/checkpoints/stack_seq2seq','save dir')
+flags.DEFINE_string('tensorboard_dir','../data/tensorboard/stack_seq2seq','tensorboard path')
+flags=flags.FLAGS
+
+class Config:
+    init_scale=0.05
+    batch_size=1
+    learning_rate=1e-3
+    input_num_steps=None
+    output_num_steps=None
+    encoder_vocab_size=None
+    decoder_vocab_size=None
+    hidden_size=100
+    embedding_size=100
+    num_epochs=50
+    START_ID=435
+    num_layers=2
+    use_attention=True
+    mode='test'
+    save_per_batch=50
+    print_per_batch=100
+
+def feed_data(x_batch,y_batch):
+    feed_dict={
+        model.inputs:x_batch,
+        model.outputs:y_batch,
+    }
+    return feed_dict
+
+def get_time_dif(start_time):
+    end_time=time.time()
+    time_dif=end_time-start_time
+    return timedelta(seconds=int(round(time_dif)))
+
+
+def evaluate(sess,x_,y_):
+    data_len=len(x_)
+    eval_batch=batch_iter(x_,y_,config.batch_size)
+    total_loss=0.0
+    total_acc=0.0
+    for x_batch,y_batch in eval_batch:
+        batch_len=len(x_batch)
+        feed_dict=feed_data(x_batch,y_batch)
+        loss,acc=sess.run([model.loss,model.acc],feed_dict=feed_dict)
+        # loss,acc=sess.run([model.loss,model.acc],feed_dict=feed_dict)
+        total_loss+=loss*batch_len
+        total_acc+=acc*batch_len
+    # return total_loss/data_len
+    return total_loss/data_len,total_acc/data_len
+
+
+
+def train(id_to_word):
+    tensorboard_dir=flags.tensorboard_dir
+    if not os.path.exists(tensorboard_dir):
+        os.makedirs(tensorboard_dir)
+    tf.summary.scalar('loss',model.loss)
+    # tf.summary.scalar('accuracy',model.acc)
+    merged_summary=tf.summary.merge_all()
+    writer=tf.summary.FileWriter(tensorboard_dir)
+
+    saver=tf.train.Saver()
+    if not os.path.exists(flags.save_dir):
+        os.makedirs(flags.save_dir)
+    Config = tf.ConfigProto()
+    Config.gpu_options.allow_growth = True
+    with tf.Session(config=Config) as sess:
+        sess.run(tf.global_variables_initializer())
+        writer.add_graph(sess.graph)
+        print('training and evaluating...')
+        start_time=time.time()
+        total_batch=0
+        best_eval_acc=0.0
+        last_improved=0
+        required_improvement=1000 #超过1000轮未提升则提前结束训练
+        flag=False
+        for epoch in range(config.num_epochs):
+            print('Epoch:',epoch+1)
+            train_batch=batch_iter(train_inputs,train_outputs,config.batch_size)
+            for x_batch,y_batch in train_batch:
+                feed_dict=feed_data(x_batch,y_batch)
+                if total_batch%config.save_per_batch==0:
+                    s=sess.run(merged_summary,feed_dict=feed_dict)
+                    writer.add_summary(s,total_batch)
+                if total_batch%config.print_per_batch==0:
+                    loss,prediction,acc,temp,correct=sess.run([model.loss,model.decoder_prediction,model.acc,model.temp,model.correct],feed_dict=feed_dict)
+                    # loss,acc,prediction=sess.run([model.loss,model.acc,model.predict_class],feed_dict=feed_dict)
+                    loss_val,acc_val=evaluate(sess,val_inputs,val_outouts)
+                    # if acc_val>best_eval_acc:
+                    #     best_eval_acc=acc_val
+                    #     last_improved=total_batch
+                    #     saver.save(sess,flags.save_path)
+                    #     improved_str='*'
+                    # else:
+                    #     improved_str=''
+                    time_dif=get_time_dif(start_time)
+                    print(temp)
+                    print(correct)
+                    print('train loss: %.3f, train acc: %.3f, val loss: %.3f, train acc: %.3f'%(loss,acc,loss_val,acc_val))
+                    targets=[]
+                    for i in y_batch[0]:
+                        targets.append(id_to_word[i])
+                    predict=[]
+                    for i in prediction[0]:
+                        predict.append(id_to_word[i])
+                    print(targets)
+                    print(predict)
+                    print('---------------------------------------------------------\n\n')
+
+                    # for i in range(2):#print 2 个example （≤batch_size）
+                    #     input_=''
+                    #     target_=''
+                    #     predic_=''
+                    #     for j in range(config.num_steps-1):
+                    #         input_+=id_to_word[x_batch[i][j]]+' '
+                    #         target_+=id_to_word[y_batch[i][j]]+' '
+                    #         predic_+=id_to_word[prediction[i][j]]+' '
+                    #     print('input:   %s\n\nTarget:   %s\n\nPrediction: %s \n\n'%(input_,target_,predic_))
+
+                sess.run(model.train_op,feed_dict=feed_dict)
+                total_batch+=1
+
+
+import json
+def test():
+    Config = tf.ConfigProto()
+    Config.gpu_options.allow_growth = True
+    with tf.Session(config=Config) as sess:
+        sess.run(tf.global_variables_initializer())
+        saver=tf.train.Saver()
+        saver.restore(sess,flags.save_path)
+        test_loss,test_acc=evaluate(sess,test_inputs,test_outputs)
+        msg='Test Loss:{0:>6.2}, Test Acc:{1:>7.2%}'
+        print(msg.format(test_loss,test_acc))
+
+
+
+
+if __name__ == '__main__':
+    config=Config()
+    print('start fetching data...')
+    encoder_word_to_id=read_vocab('../data/atis/vocab.en.txt')
+    decoder_word_to_id=read_vocab('../data/atis/vocab.de.txt')
+    decoder_word_to_id['START']=len(decoder_word_to_id)
+    train_inputs,train_outputs,val_inputs,val_outouts,test_inputs,test_outputs,left_id, right_id, EN_PAD_ID,DE_PAD_ID,encoder_numsteps,decoder_numsteps=raw_data('../data/atis',encoder_word_to_id,decoder_word_to_id)
+    config.input_num_steps=encoder_numsteps
+    config.output_num_steps=decoder_numsteps
+    config.encoder_vocab_size=len(encoder_word_to_id)
+    config.decoder_vocab_size=len(decoder_word_to_id)
+    id_to_word=reverseDic(decoder_word_to_id)
+
+    model=Stack_Seq2seqModel(config,left_id,right_id)
+
+    if config.mode=='train':
+        train(id_to_word)
+    else:
+        test()
--- model/stack_seq2seq.py
+++ model/stack_seq2seq.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import tensorflow as tf
 
-class Seq2seqModel:
-    def __init__(self,config):
+class Stack_Seq2seqModel:
+    def __init__(self,config,left_id, right_id):
         self.config=config
         self.inputs=tf.placeholder(tf.int64,[config.batch_size,self.config.input_num_steps],name='inputs')
         self.outputs=tf.placeholder(tf.int64,[config.batch_size,self.config.output_num_steps],name='outputs')
@@ -18,7 +18,8 @@ class Seq2seqModel:
         self.encoder_outputs,self.encoder_final_state=self.add_encoder(self.embed_inputs)
         self.decoder_logits,self.decoder_prediction,self.temp,self.correct,self.acc=self.add_decoder(self.encoder_final_state,self.encoder_outputs,self.embed_outputs)
         self.loss,self.train_op=self.train(self.outputs,self.decoder_logits)
-
+        self.start=left_id
+        self.end=right_id
     def add_encoder(self,encoder_inputs):
         with tf.variable_scope('encoder'):
             def create_cell(hidden_size):
@@ -101,3 +102,84 @@ class Seq2seqModel:
         return loss,train_op
 
 
+    def _build_rnn_graph_lstm(self, inputs, config):
+        """Build the inference graph using canonical LSTM cells."""
+        def make_cell():
+            cell = tf.contrib.rnn.BasicLSTMCell(config.hidden_size, forget_bias=0.0, state_is_tuple=True)
+            cell = tf.contrib.rnn.DropoutWrapper(cell, output_keep_prob=config.dropout_keep_prob)
+            return cell
+
+        cell = tf.contrib.rnn.MultiRNNCell(
+            [make_cell() for _ in range(config.num_layers)], state_is_tuple=True)
+
+        self._initial_state = cell.zero_state(config.batch_size, tf.float32)
+        state = self._initial_state
+
+        outputs = []
+
+        def func_push(state):
+            self.state_stack.push(state)
+            return state[0][0],state[0][1],state[1][0],state[1][1]
+
+
+        #-----------------特殊情况需要保留名称------------------
+        # def updateState(state,time_step):
+        #     # state = ((state[0][0], state[0][1]), (state[1][0],state[1][1]))
+        #
+        #     (out, newstate) = cell(inputs[:, time_step, :], state)
+        #     # print('------------------------------hhhhhh----------------------------')
+        #     tf.get_variable_scope().reuse_variables()
+        #     return newstate
+        # nameSet=[word_to_id['Import'],word_to_id['ClassDef'],word_to_id['FunctionDef'],word_to_id['Assign'],word_to_id['AsyncFunctionDef'],word_to_id['Attribute']]
+        # def f_default(state):
+        #     return state,state
+        #
+        # def func_push(state, time_step):
+        #     #add特殊情况需要保留名称
+        #     state,newState = tf.cond(tf.logical_or(
+        #         tf.logical_or(tf.equal(self._input_data[0][time_step-1], nameSet[0]), tf.equal(self._input_data[0][time_step-1], nameSet[1])),
+        #         tf.logical_or(tf.equal(self._input_data[0][time_step-1], nameSet[2]), tf.equal(self._input_data[0][time_step-1], nameSet[3])),
+        #     ),lambda: updateState(state, time_step), lambda: f_default(state))
+        #
+        #     self.state_stack.push(newState)
+        #     return state[0][0], state[0][1], state[1][0], state[1][1]
+        # #-------------------------------------------------------------
+
+        def func_pop(time_step,state):
+            (cell_output,state)=cell(inputs[:,time_step,:],state)
+            state=self.state_stack.pop()
+            (cell_output,state)=cell(cell_output,state)
+            return state[0][0],state[0][1],state[1][0],state[1][1]
+        #def func_pop(state):
+        #    #------增加一层---------------#
+        #    w = tf.get_variable(
+        #        "state_w", [2*config.hidden_size, config.hidden_size], dtype=tf.float32)
+        #    # b = tf.get_variable("state_b", [1,config.hidden_size], dtype=data_type())
+        #    old_state=self.state_stack.pop()
+        #    concat_state=[[],[]]
+        #    new_state=[[],[]]
+        #    # concat_state=tf.concat([old_state,state],1)
+        #    # concat_state=tf.reshape(concat_state,[1,-1])
+        #    for i in range(2):
+        #        for j in range(2):
+        #            concat_state[i].append(tf.concat([old_state[i][j],state[i][j]],1))
+        #            new_state[i].append(tf.matmul(concat_state[i][j], w))
+        #    return new_state[0][0], new_state[0][1], new_state[1][0], new_state[1][1]
+
+        def func_default(state):
+            return state[0][0],state[0][1],state[1][0],state[1][1]
+
+        with tf.variable_scope("RNN"):
+            for time_step in range(self.config.num_steps):
+                if time_step > 0: tf.get_variable_scope().reuse_variables()
+                # print(self.input)
+                new_state=tf.cond(tf.equal(self.embed_inputs[0][time_step],self.start),
+                                  lambda:func_push(state),lambda:func_default(state))
+                new_state=tf.cond(tf.equal(self.embed_inputs[0][time_step],self.end),
+                                  lambda:func_pop(time_step,state),lambda:func_default(state))
+                state=((new_state[0],new_state[1]),(new_state[2],new_state[3]))
+
+                (outputs, state) = cell(inputs[:, time_step, :], state)
+                #outputs.append(cell_output)
+        # output = tf.reshape(tf.concat(outputs, 1), [-1, config.hidden_size])
+        return outputs, state
